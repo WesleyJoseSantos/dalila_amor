16a1ceaa875847ce6a3628363f3c6c3a3ea36238
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2022-05-15 16:47:37 -0300

    feat: github pages changes

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0a4fb1326c8d987bc2d9b4b6a1f05bf4",
-"/": "0a4fb1326c8d987bc2d9b4b6a1f05bf4",
-"main.dart.js": "117d2769c9f12de60e249ca11884156d",
+"index.html": "6aa13b85f142fb558440c03b199a819c",
+"/": "6aa13b85f142fb558440c03b199a819c",
+"main.dart.js": "a42de75e0565b895e9cdcd2533c3befb",
 "manifest.json": "0a7af4b1f5092079d39959285a552f70",
 "version.json": "0fe98452027128aa8e066d2b21417f16"
 };
